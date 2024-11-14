fb470ffe9b84d9e9c5fc6b3b19ee07050d03f9fe
r1317 <122949375+r-1317@users.noreply.github.com>
2024-11-14 10:33:12 +0900

    1114-1

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { twMerge } from "tailwind-merge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFish } from "@fortawesome/free-solid-svg-icons";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="bg-slate-800 py-2">
+        <div
+          className={twMerge(
+            "mx-4 max-w-2xl md:mx-auto",
+            "flex items-center justify-between",
+            "text-lg font-bold text-white"
+          )}
+        >
+          <div>
+            <FontAwesomeIcon icon={faFish} className="mr-1" />
+            Header
+          </div>
+          <div>About</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
