560440220e4c7e848cdb085c8a0ada87a50b0397
r1317 <122949375+r-1317@users.noreply.github.com>
2024-11-21 10:30:33 +0900

    1121-1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "w1980.blob.core.windows.net",
+      },
+      { protocol: "https", hostname: "placehold.jp" },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/_components/PostSummary.tsx
+++ src/app/_components/PostSummary.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { Post } from "@/app/_types/Post";
+import Link from "next/link";
 
 type Props = {
   post: Post;
@@ -11,13 +12,20 @@ const PostSummary: React.FC<Props> = (props) => {
     <div className="border border-slate-400 p-3">
       {post.createdAt.substring(0, 10)}
       {post.categories.map((category) => (
-        <span key={category.id} className="ml-2 text-gray-500 border border-gray-300 p-1 rounded">
+        <span
+          key={category.id}
+          className="ml-2 rounded border border-gray-500 p-1 text-gray-500"
+        >
           {category.name}
         </span>
       ))}
-      <div className="font-bold">{post.title}</div>
-      {/* <div>{post.content}</div> */}
-      <div dangerouslySetInnerHTML={{ __html: post.content }} className= "line-clamp-3" />
+      <Link href={`/posts/${post.id}`}>
+        <div className="mb-1 text-lg font-bold">{post.title}</div>
+        <div
+          className="line-clamp-3"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </Link>
     </div>
   );
 };
