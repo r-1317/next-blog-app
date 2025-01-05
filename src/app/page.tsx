3b531ae37c3cd023a8790c6e03e727bd8eeaea98
r1317 <122949375+r-1317@users.noreply.github.com>
2025-01-05 22:56:48 +0900

    0105-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import type { Post } from "@/app/_types/Post";
+import type { PostApiResponse } from "@/app/_types/PostApiResponse";
 import PostSummary from "@/app/_components/PostSummary";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
@@ -17,19 +18,36 @@ const Page: React.FC = () => {
     const fetchPosts = async () => {
       try {
         // microCMS から記事データを取得
-        const requestUrl = `${apiBaseEp}/posts`;
+        // const requestUrl = `${apiBaseEp}/posts`;
+        const requestUrl = `/api/posts`;
         const response = await fetch(requestUrl, {
           method: "GET",
           cache: "no-store",
-          headers: {
-            "X-MICROCMS-API-KEY": apiKey,
-          },
+          // headers: {
+          //   "X-MICROCMS-API-KEY": apiKey,
+          // },
         });
         if (!response.ok) {
           throw new Error("データの取得に失敗しました");
         }
-        const data = await response.json();
-        setPosts(data.contents as Post[]);
+        const postResponse: PostApiResponse[] = await response.json();
+        setPosts(
+          postResponse.map((rawPost) => ({
+            id: rawPost.id,
+            title: rawPost.title,
+            content: rawPost.content,
+            coverImage: {
+              url: rawPost.coverImageURL,
+              width: 1000,
+              height: 1000,
+            },
+            createdAt: rawPost.createdAt,
+            categories: rawPost.categories.map((category) => ({
+              id: category.category.id,
+              name: category.category.name,
+            })),
+          }))
+        );
       } catch (e) {
         setFetchError(
           e instanceof Error ? e.message : "予期せぬエラーが発生しました"
